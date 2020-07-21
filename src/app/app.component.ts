eae2daa5dc5015a078078a66b5829a4ffa18b944
Mrinal Kanti. Mondal <mkmondal@oradev.oraclecorp.com>
2020-07-21 17:53:10 +0530

    commented out social login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,29 @@ import { Router } from '@angular/router';
 
 import { AuthenticationService } from './_services';
 import { User } from './_models';
+/*import { SocialLoginModule, SocialAuthServiceConfig } from 'angularx-social-login';
+import {
+  LinkedinLoginProvider
+} from 'angularx-social-login';
+@NgModule({
+    imports: [ SocialLoginModule ],
+    providers: [
+      {
+        provide: 'SocialAuthServiceConfig',
+        useValue: {
+          autoLogin: false,
+          providers: [
+            {
+              id: LinkedinLoginProvider.PROVIDER_ID,
+              provider: new LinkedinLoginProvider(
+                'clientId'
+              ),
+            }
+          ],
+        } as SocialAuthServiceConfig,
+      }
+    ]
+  })*/
 
 @Component({ selector: 'app', templateUrl: 'app.component.html' })
 export class AppComponent {
